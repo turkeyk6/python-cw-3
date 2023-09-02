c2fabc8023b24004f3b1e8bd24a1f8338aeca0e2
turkeyk6 <142680770+turkeyk6@users.noreply.github.com>
2023-09-02 19:16:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # write your code here
+fav_animals = ["dog" , "cat" , "monkey" , "rabbet"]
+print(fav_animals)
+print(fav_animals[3])
+fav_animals.remove("monkey")
+fav_animals.append("cow")
+fav_animals.append("monkey")
+for animal in fav_animals :
+    print (f'i love {animal}')
+
+    numbers = [5, 4 , 3 , 2 , 1]
+    numbers_num = 0
+    for mum in numbers :
+        numbers_num += num
+
+
+        print(numbers_num)
